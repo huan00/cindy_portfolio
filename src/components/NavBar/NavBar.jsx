7842d604e3f480e3c9c22b7e82ebca218d07a1a2
huan00 <96999144+huan00@users.noreply.github.com>
2022-09-11 15:47:24 -0400

    set up navbar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div>
+      <h6>Cindy Leong</h6>
+      <p>Avaliable for Projects // Say Hello</p>
+      <h6>New York, NY // 10:54 AM EST</h6>
+    </div>
+  )
+}
+
+export default NavBar
---
+++ src/components/ProjectBlock/ProjectBlock.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './styles.css'
+
+const ProjectBlock = () => {
+  return (
+    <div className="projectBlock" style={{ width: '300px', height: '350px' }}>
+      <div>
+        <h4>Project Name:</h4>
+      </div>
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae,
+          ratione quibusdam? Laudantium, minima illo ipsa ullam laborum at, ea
+          nesciunt nostrum quibusdam vel blanditiis deleniti suscipit ducimus
+          rerum commodi magnam?
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default ProjectBlock
